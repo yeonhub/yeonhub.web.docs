a4bd02f0a2525ea2cb1a7ce0c06f84c63ebe92fc
yeonhub <lsy3237@gmail.com>
2023-04-06 12:52:38 +0900

    [ADD] javascript Day-012

---
+++ 02_javascript(basic)/12일차/01_논리.js
@@ -0,0 +1,13 @@
+// 논리연산자 - true / false 두 가지만 결과로
+//  && 논리곱(and), || 논리합(or), ! 부정(not)
+// 관계연산자
+console.log( (10>5) && (15>20));
+// false
+console.log( (10>5) || (15>20));
+// true
+console.log( !(10>5));
+// false
+console.log( !false );
+// true
+console.log(!true);
+// false
