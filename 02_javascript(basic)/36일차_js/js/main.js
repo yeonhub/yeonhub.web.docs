a50b2dde82879e0dca9c0165b2b29ed887921d76
yeonhub-camp <lsy3237@gmail.com>
2023-05-02 09:37:26 +0900

    [ADD] javascript Day-036

---
+++ 02_javascript(basic)/36일차_js/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+// 즉시실행함수
+(function(){
+    // 안에 코드 작성
+    const get = (target) => {
+        return document.querySelector(target);
+    }
+    const getAll = (target) => {
+        return document.querySelector(target);
+    }
+})();
