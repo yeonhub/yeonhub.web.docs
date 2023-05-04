16a0dd4ffdfcb5c4e1e77fad8f8e83582c7ae5a5
yeonhub-camp <lsy3237@gmail.com>
2023-05-04 09:28:59 +0900

    [ADD] javascript Day-038

---
+++ 02_javascript(basic)/38일차_js/메뉴/js/menu3.js
@@ -0,0 +1,28 @@
+const get =(target)=>{
+  return document.querySelector(target);
+}
+const getAll =(target)=>{
+  return document.querySelectorAll(target);
+}
+
+let $gnbli = getAll('#header .nav > .gnb > li');
+let $subli = getAll('#header .nav .gnb li ul');
+let $header = get('#header');
+
+// li에 마우스 올리면 자식 중 두번째에 on 붙이기
+
+$gnbli.forEach(item => {
+  item.addEventListener('mouseenter', e=>{
+    $header.classList.add('on');
+    $subli.forEach(subItem=>{
+      subItem.classList.remove('on');
+    })
+    e.currentTarget.children[1].classList.add('on')
+  })  
+});
+$header.addEventListener('mouseleave', e=>{
+  $subli.forEach(subItem=>{
+    subItem.classList.remove('on');
+    $header.classList.remove('on');
+  })
+})
